fc931280269f6f285db14b9472c64625c60f3182
liwei <liwei72439936@gmail.com>
2018-03-20 22:36:51 +0800

    fix spi not consumer space and read integer error

--- spi.hs
+++ spi.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Data.Foldable
 import Data.Char
 import Data.Function
+import Data.Maybe
 
 newtype Identity a = Identity { runIdentity :: a } deriving Functor
 
@@ -15,7 +16,7 @@ instance Monad Identity where
   return = Identity
   ia >>= f = f $ runIdentity ia
 
-data ParserT m a = ParserT {runParserT :: String -> m (Maybe (a,String))}
+data ParserT m a = ParserT {runParserT :: String -> m (Maybe a,String)}
   deriving Functor
 
 type Parser = ParserT Identity
@@ -25,18 +26,18 @@ instance (Monad m) => Applicative (ParserT m) where
   (<*>) = ap
 
 instance (Monad m) => Monad (ParserT m) where
-  return a = ParserT $ \s -> return $ Just (a,s)
-  mpa >>= f = ParserT $ \s -> do pa <- runParserT mpa s
+  return a = ParserT $ \s -> return $ (Just a,s)
+  mpa >>= f = ParserT $ \s -> do (pa, s') <- runParserT mpa s
                                  case pa of
-                                   Just (a,s') -> runParserT (f a) s'
-                                   Nothing -> return Nothing
+                                   Just a -> runParserT (f a) s'
+                                   Nothing -> return (Nothing, s')
 
 instance (Monad m) => Alternative (ParserT m) where
-  empty = ParserT $ \_ -> return Nothing
-  pa <|> pb = ParserT $ \s -> do x <- runParserT pa s
+  empty = ParserT $ \s -> return (Nothing,s)
+  pa <|> pb = ParserT $ \s -> do w@(x,_) <- runParserT pa s
                                  case x of
                                    Nothing -> runParserT pb s
-                                   x -> return x
+                                   x -> return w
 
 type Identifier = String
 data Op = Plus | Minus | Mul | Div deriving (Show, Eq)
@@ -54,8 +55,8 @@ data Var = Var Identifier deriving Show
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy cond = ParserT $ \s ->
   return $ case s of
-             x:xs -> if (cond x) then Just (x,xs) else Nothing
-             _ -> Nothing
+             x:xs -> if (cond x) then (Just x,xs) else (Nothing, s)
+             _ -> (Nothing, s)
 
 char :: Char -> Parser Char
 char c = satisfy (c==)
@@ -128,8 +129,8 @@ assignmentStatement = do
 
 variable :: Parser Var
 variable = do
-  f <- satisfy (\c -> isLetter c || c == '_')
-  left <- many $ satisfy isLetter
+  f <- token $ satisfy (\c -> isLetter c || c == '_')
+  left <- many $ token $ satisfy isLetter
   return $ Var (f:left)
 
 factor :: Parser Expr
@@ -140,8 +141,8 @@ factor =
       return v)
   <|> (token $ char '+' >> (UnaryOp Pos) <$> expr)
   <|> (token $ char '-' >> (UnaryOp Neg) <$> expr)
-  <|> (ExprVal . read) <$> (token $ many $ satisfy isDigit)
   <|> (ExprVar <$> variable)
+  <|> (ExprVal . read) <$> (token $ many $ satisfy isDigit)
 
 expr :: Parser Expr
 expr = do
@@ -163,11 +164,12 @@ term = do
                     '/' -> return (\i -> BinOp i Div r))
   return $ foldl (flip ($)) l opt
 
-parse :: String -> Identity Expr
-parse ip = do y <- runParserT expr ip
+parse :: String -> Identity Program
+parse ip = do y <- runParserT program ip
               case y of
-                Just (x, _) -> return x
-                Nothing -> error "Parser error"
+                (Just x, "") -> return x
+                (Just _, left) -> error $ "not consume " ++ left
+                (Nothing, left) -> error $ "error when parse" ++ left
 
 interpret :: Expr -> Int
 interpret (ExprVal x) = x
@@ -185,4 +187,5 @@ main = forever $
   do
     putStr "spi> "
     l <- getLine
-    putStrLn $ show $ interpret $ runIdentity $ parse l
+    return ()
+--    putStrLn $ show $ interpret $ runIdentity $ parse l
